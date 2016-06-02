3cb79655136b5d02ffe8ca7f29ebd2c35b3dcf91
kyuhlee <kyuhyung.lee@gmail.com>
2016-06-02 06:05:54 -0700

    bug fixw

--- scripts/auditlog.sh
+++ scripts/auditlog.sh
@@ -4,10 +4,11 @@ echo "Killing auditd..."
 sudo killall -9 auditd
 
 echo "Copy audit log..."
-sudo mv /var/log/audit/audit.log ./audit_ori.log
+sudo ausearch -r > audit_ori.log
+#sudo mv /var/log/audit/audit.log ./audit_ori.log
 
 echo "Sort audit log..."
-sudo auditlog_sort ./audit_ori.log > audit.log
+auditlog_sort ./audit_ori.log > audit.log
 
 echo "audit.log is ready..."
 
