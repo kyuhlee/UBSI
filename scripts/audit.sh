f155ee8c171365d8d7227ef99bb8cb2d39a9efee
kyuhlee <kyuhyung.lee@gmail.com>
2016-07-13 00:09:44 -0400

    Add dup, dup2, dup3 syscalls in audit.sh

--- scripts/audit.sh
+++ scripts/audit.sh
@@ -33,7 +33,7 @@ echo "Cleaning up auditd rules..."
 _sudo auditctl -D || fail 4
 
 echo "Applying new set of rules to auditd..."
-_sudo auditctl -a exit,always -S 1 -S 2 -S 3 -S 18 -S 20 -S 296 -S 41 -S 42 -S 43 -S 44 -S 45 -S 46 -S 47 -S 56 -S 57 -S 58 -S 59 -S 2 -S 293 -S 22 -S 288 -S 0 -S 17 -S 19 -S 295 -S 82 -S 264 -S 87 -S 86 -S 62 -S 9 -S 10 -F uid=$USER_ID || fail 5
+_sudo auditctl -a exit,always -S 1 -S 2 -S 3 -S 18 -S 20 -S 296 -S 41 -S 42 -S 43 -S 44 -S 45 -S 46 -S 47 -S 56 -S 57 -S 58 -S 59 -S 2 -S 293 -S 22 -S 288 -S 0 -S 17 -S 19 -S 295 -S 82 -S 264 -S 87 -S 86 -S 62 -S 9 -S 10 -S 32 -S 33 -S 292 -F uid=$USER_ID || fail 5
 
 echo "ok"
 
